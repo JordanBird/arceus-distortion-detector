488b872db73a0916a01cc4da68ac9c47d277ae29
Jordan Bird <jordanbird-29@hotmail.co.uk>
2023-05-06 18:00:29 +0100

    Add timer support and flesh out help text.

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let stream;
 let recorder;
 
 let isWatching = false;
+let detected = false;
+let timersRunning = false;
 
 function initEvents() {
   $('.detect-toggle').on('click', async function (e) {
@@ -34,7 +36,31 @@ function initEvents() {
   $('.dimiss-alert').on('click', function (e) {
     e.preventDefault();
 
-    $('.detector-alert').hide();
+    dismissDetection();
+  });
+
+  $('.toggle-timer').on('click', function (e) {
+    e.preventDefault();
+
+    timersRunning = !timersRunning;
+
+    if (!timersRunning) {
+      $(this).html("Start Timer");
+
+      return;
+    }
+    
+    $(this).html("Pause Timer");
+
+    startTimers();
+  });
+
+  $('.stop-timer').on('click', function (e) {
+    e.preventDefault();
+
+    timersRunning = false;
+
+    initTimers();
   });
 }
 
@@ -49,7 +75,7 @@ function doTesStream() {
       console.log(text);
 
       if (isSpaceTime(text)) {
-        $('.detector-alert').show();
+        onDetected();
       }
 
       if (isWatching) {
@@ -87,8 +113,101 @@ function isSpaceTime(match) {
   return match.toUpperCase().indexOf("SPACE") > -1 && match.toUpperCase().indexOf("TIME") > -1;
 }
 
+function initTimers() {
+  var timers = $('[data-countdown]');
+  for (var i = 0; i < timers.length; i++) {
+    $(timers[i]).data('countdown-elapsed', 0);
+
+    var totalTime = parseInt($(timers[i]).data('countdown'));
+    var elapsedTime = parseInt($(timers[i]).data('countdown-elapsed'));
+
+    $(timers[i]).html(getTimerText(totalTime, elapsedTime));
+  }
+
+  $('.toggle-timer').html("Start Timer");
+}
+
+function startTimers() {
+  timersRunning = true;
+
+  var timers = $('[data-countdown]');
+  for (var i = 0; i < timers.length; i++) {
+    tickTimer(timers[i]);
+  }
+}
+
+function tickTimer(element) {
+  if (!timersRunning) {
+    return;
+  }
+
+  var totalTime = parseInt($(element).data('countdown'));
+  var elapsedTime = parseInt($(element).data('countdown-elapsed'));
+
+  $(element).html(getTimerText(totalTime, elapsedTime));
+
+  $(element).data('countdown-elapsed', elapsedTime + 1);
+
+  setTimeout(function () {
+    tickTimer(element);
+  }, 1000);
+}
+
+function getTimerText(totalTimeInMinutes, elapsedTimeInSeconds) {
+  var remainingTime = (totalTimeInMinutes * 60) - elapsedTimeInSeconds;
+  if (remainingTime <= 0) {
+    return "00:00";
+  }
+  
+  time = new Date(remainingTime * 1000);
+  hours = time.getUTCHours();
+  mins = time.getUTCMinutes();
+
+  return (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+}
+
+function twoDigits(n)
+{
+    return (n <= 9 ? "0" + n : n);
+}
+
+function onDetected() {
+  detected = true;
+
+  alertUser();
+}
+
+function dismissDetection() {
+  detected = false;
+
+  $('.detector-alert').hide();
+
+  document.title = "Space-time Distortions Detector";
+}
+
+function alertUser() {
+  if (!detected) {
+    return;
+  }
+
+  $('.detector-alert').show();
+
+  var text = "Space-time Distortions Detector";
+  if (new Date().getSeconds() % 2 == 0) {
+    text = "--DETECTED--";
+  }
+
+  document.title = text;
+
+  setTimeout(function () {
+    alertUser();
+  }, 500);
+}
+
 function init() {
   initEvents();
+
+  initTimers();
 }
 
 $(document).ready(init);
