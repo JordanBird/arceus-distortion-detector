5a5fe09e8f2445929912791c948091c1194fcaf6
Jordan Bird <jordanbird-29@hotmail.co.uk>
2023-05-21 04:36:29 +0100

    * Add Dynamic Timer * Improve detection * Optimise detection * Improve styling

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const video = document.querySelector('video');
 const canvas = document.querySelector("canvas");
 
+let worker;
+
 let stream;
 let recorder;
 
@@ -8,12 +10,76 @@ let isWatching = false;
 let detected = false;
 let timersRunning = false;
 
-let dynamicTimerRunning = false;
+let dynamicTimerRunning = true;
 let dynamicTimerPause = false;
 
 let defaultScreenRatio = 3;
 let dynamicTimerScreenRatio = 1;
 
+let rectangles = [
+  {
+    name: "Space Time Distortion",
+    rect: { left: 25, top: 10, width: 50, height: 15 },
+    lookups: {
+      all: [],
+      any: [ "SPACE", "TIME", "DISTORTION" ],
+      none: [ "SATCHEL", "ITEM", "FADED" ]
+    },
+    mode: "Space Time",
+    dynamic: false
+  },
+  {
+  name: "Overworld",
+  rect: { left: 3, top: 75, width: 5, height: 8 },
+  lookups: {
+    all: [ "VO" ], //The dash we're monitoring is picked up as VO, so for now, we match VO
+    any: [],
+    none: []
+  },
+  mode: "Overworld",
+  dynamic: true
+},
+{
+  name: "Bottom Right",
+  rect: { left: 75, top: 95, width: 25, height: 5 },
+  lookups: {
+    all: [],
+    any: [ "EXIT", "BACK", "QUIT" ],
+    none: []
+  },
+  mode: "Menu",
+  dynamic: true
+},
+{
+  name: "Top Left",
+  rect: { left: 0, top: 0, width: 25, height: 7 },
+  lookups: {
+    all: [],
+    any: [ "SATCHEL", "SAVE", "COMMUNICATIONS", "HELP" ],
+    none: []
+  },
+  mode: "Menu",
+  dynamic: true
+},
+{
+  name: "Battle",
+  rect: { left: 7, top: 82, width: 10, height: 7 },
+  lookups: {
+    all: [],
+    any: [ "LV." ],
+    none: []
+  },
+  mode: "Battle",
+  dynamic: true
+}];
+
+let currentMode = "";
+
+async function initWorker() {
+  worker = await Tesseract.createWorker();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+}
 
 function initEvents() {
   $('.detect-toggle').on('click', async function (e) {
@@ -75,50 +141,127 @@ function initEvents() {
 
     dynamicTimerRunning = $(this).is(":checked");
   });
+
+  $('.toggle-debug-visibility').on('click', function (e) {
+    e.preventDefault();
+
+    toggleDebugControls();
+  });
 }
 
-function startTesseract() {
+async function startTesseract() {
   if (!isWatching) {
     return;
   }
 
-  Tesseract.recognize(canvas, 'eng', { logger: m => $('.parseProgress').html(m.progress) }).then(processParsedText);
-}
+  var anyMatch = false;
+  for (let i = 0; i < rectangles.length; i++) {
+    if (!dynamicTimerRunning && rectangles[i].dynamic) {
+      continue;
+    }
 
-function processParsedText(match) {
-  $('.parsedText').html(match.data.text);
+    let result;
+    if (dynamicTimerRunning) {
+      result = await worker.recognize(canvas, { rectangle: percentageRectangleToTesseractRectangle(rectangles[i].rect) });
+    }
+    else {
+      result = await worker.recognize(canvas);
+    }
 
-  if (dynamicTimerRunning && isBattle(match.data.text)) {
-    $('.tracker-card').removeClass('menu-mode');
+    var matched = processText(result.data.text, rectangles[i].lookups);
+    if (matched) {
+      anyMatch = true;
 
-    $('.tracker-card').addClass('battle-mode');
-
-    dynamicTimerPause = true;
+      updateMode(rectangles[i].mode);
+    }
   }
-  else if (dynamicTimerRunning && isMenu(match.data.text)) {
-    $('.tracker-card').removeClass('battle-mode');
 
-    $('.tracker-card').addClass('menu-mode');
-
-    dynamicTimerPause = true;
-  }
-  else if (isSpaceTime(match.data.text)) {
-    $('.tracker-card').removeClass('battle-mode');
-    $('.tracker-card').removeClass('menu-mode');
-
-    dynamicTimerPause = false;
-
-    onDetected();
-  }
-  else {
-    $('.tracker-card').removeClass('battle-mode');
-    $('.tracker-card').removeClass('menu-mode');
-
-    dynamicTimerPause = false;
+  if (!anyMatch) {
+    //Do something, maybe game isn't running? Cutscene? Loading? Accurate enough to stop timer in this case?
+    //updateMode("Unknown");
   }
 
   if (isWatching) {
     watchTick();
+  }
+}
+
+function processText(text, lookups) {
+  text = text.toUpperCase();
+
+  for (let i = 0; i < lookups.none.length; i++) {
+    if (text.indexOf(lookups.none[i]) > -1) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < lookups.all.length; i++) {
+    if (text.indexOf(lookups.all[i]) == -1) {
+      return false;
+    }
+  }
+
+  if (lookups.all.length > 0) {
+    return true;
+  }
+
+  for (let i = 0; i < lookups.any.length; i++) {
+    if (text.indexOf(lookups.any[i]) > -1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function updateMode(mode) {
+  if (currentMode == mode) {
+    return;
+  }
+
+  currentMode = mode;
+
+  switch (mode) {
+    case "Space Time":
+      $('.tracker-card').removeClass('battle-mode');
+      $('.tracker-card').removeClass('unknown-mode');
+      $('.tracker-card').removeClass('menu-mode');
+
+      dynamicTimerPause = false;
+
+      onDetected();
+      return;
+    case "Overworld":
+      $('.tracker-card').removeClass('battle-mode');
+      $('.tracker-card').removeClass('unknown-mode');
+      $('.tracker-card').removeClass('menu-mode');
+
+      dynamicTimerPause = false;
+      return;
+    case "Menu":
+      $('.tracker-card').removeClass('battle-mode');
+      $('.tracker-card').removeClass('unknown-mode');
+      $('.tracker-card').addClass('menu-mode');
+  
+      dynamicTimerPause = true;
+      return;
+    case "Battle":
+      $('.tracker-card').removeClass('menu-mode');
+      $('.tracker-card').removeClass('unknown-mode');
+      $('.tracker-card').addClass('battle-mode');
+
+      dynamicTimerPause = true;
+      return;
+    case "Unknown":
+      $('.tracker-card').removeClass('menu-mode');
+      $('.tracker-card').removeClass('battle-mode');
+      $('.tracker-card').addClass('unknown-mode');
+
+      dynamicTimerPause = false;
+      return;
+    default:
+        dynamicTimerPause = false;
+        return;
   }
 }
 
@@ -140,7 +283,7 @@ function watchTick() {
     }
 
     new ImageCapture(tracks[0]).grabFrame()
-    .then((imageBitmap) => {
+    .then(async (imageBitmap) => {
       canvas.width = imageBitmap.width;
 
       if (dynamicTimerRunning) {
@@ -154,7 +297,7 @@ function watchTick() {
       canvas.getContext("2d").drawImage(imageBitmap, 0, 0);
       canvas.classList.remove("hidden");
 
-      startTesseract();
+      await startTesseract();
     })
     .catch((error) => {
       console.error("watchTick() error: ", error);
@@ -224,6 +367,14 @@ function isMenu(match) {
     { "lookup": "CAUGHT", "matched": false },
     { "lookup": "MEMBER", "matched": false }
   ]);
+
+  //Move Info
+  //Back?
+  //Yes
+  //No
+  //run away
+  //Fight
+  //Run
 
   return lookups.some(x => x.matched);
 }
@@ -346,10 +497,46 @@ function alertUser() {
   }, 500);
 }
 
+function renderDebugRectangles() {
+  $('.video-container .debug-rectangle').remove();
+
+  for (var i = 0; i < rectangles.length; i++) {
+    $('.video-container').append(
+      $('<div>')
+        .addClass('debug-toggle')
+        .addClass('debug-rectangle')
+        .css('position', 'absolute')
+        .css('top', rectangles[i].rect.top + '%')
+        .css('left', rectangles[i].rect.left + '%')
+        .css('width', rectangles[i].rect.width + '%')
+        .css('height', rectangles[i].rect.height + '%')
+        .css('background-color', 'red')
+        .css('opacity', '0.25')
+      );
+  }
+}
+
+function toggleDebugControls() {
+  $('.debug-toggle').toggle();
+}
+
+function percentageRectangleToTesseractRectangle(rect) {
+  var width = canvas.width / 100;
+  var height = canvas.height / 100;
+
+  return { left: rect.left * width, top: rect.top * height, width: rect.width * width, height: rect.height * height };
+}
+
 function init() {
+  initWorker();
+  
   initEvents();
 
   initTimers();
+
+  renderDebugRectangles();
+
+  toggleDebugControls();
 }
 
 $(document).ready(init);
